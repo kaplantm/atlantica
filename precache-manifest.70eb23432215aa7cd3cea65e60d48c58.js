cb3788b3931e436ca261656e1edf3ad572dfe49e
Toni Kaplan <tonarie.k@gmail.com>
2020-03-12 15:22:52 -0400

    Updates

---
+++ precache-manifest.70eb23432215aa7cd3cea65e60d48c58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa5d9b1f2eccf83ccfe08dfab1cd6ca7",
+    "url": "/atlantica/index.html"
+  },
+  {
+    "revision": "e2d830cb3439fd475924",
+    "url": "/atlantica/static/css/main.0aaec89d.chunk.css"
+  },
+  {
+    "revision": "97f064c5473a5a79f330",
+    "url": "/atlantica/static/js/2.05f09ad2.chunk.js"
+  },
+  {
+    "revision": "f4018ea5fe5050861ab722c30277dac5",
+    "url": "/atlantica/static/js/2.05f09ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d830cb3439fd475924",
+    "url": "/atlantica/static/js/main.480e2fe1.chunk.js"
+  },
+  {
+    "revision": "dbf1c2a998c9602af247",
+    "url": "/atlantica/static/js/runtime-main.72acabfe.js"
+  }
+]);
